2c534039233c19a73240b5fa206021f20b97282a
Rings-Rachit <86543517+Rings-Rachit@users.noreply.github.com>
2022-04-14 19:32:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function StartClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/1Sl4_5z7D/', modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error,results)
+{
+
+}
